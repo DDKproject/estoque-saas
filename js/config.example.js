c9b416b62c0f236e6028aa2573004824cc4f1953
DDKproject <caik20132013@gmail.com>
2025-08-15 09:32:28 -0300

    Update config.example.js

--- js/config.example.js
+++ js/config.example.js
@@ -1,3 +1,3 @@
 // Renomeie este arquivo para 'config.js' e preencha as chaves do seu projeto Supabase.
-export const SUPABASE_URL = "https://SEU_PROJECT_URL.supabase.co";
-export const SUPABASE_ANON_KEY = "SUA_ANON_KEY";
+export const SUPABASE_URL = "https://rmlsqrmkomkwjugmjkty.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtbHNxcm1rb21rd2p1Z21qa3R5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyNTg3NTAsImV4cCI6MjA3MDgzNDc1MH0.7EUj_QUd5ts1DwzWiKbkTMuIjubeGzc4NRRS9lAgnnk";
