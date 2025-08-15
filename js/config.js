712edc35af99fd370665cdf4b8e09fdfaedf3e78
David Caik <caik20132013@gmail.com>
2025-08-15 08:59:03 -0300

    chore: conecta front ao Supabase (URL + anon key)

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // ➜ PREENCHA AQUI com suas credenciais do Supabase e faça o deploy
-export const SUPABASE_URL = "COLOQUE_AQUI_URL";
-export const SUPABASE_ANON_KEY = "COLOQUE_AQUI_ANON_KEY";
+export const SUPABASE_URL = "https://dnpzigoowphwjwkdpyet.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRucHppZ29vd3Bod2p3a2RweWV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyNTQ2NTIsImV4cCI6MjA3MDgzMDY1Mn0.RXfOPmD8ljwI8Fj1OxS-cB2Fwn-Xd3wD_xIhXEH0Ak0";
